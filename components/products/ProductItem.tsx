2cdbaf521b2b35c0a89efeca0e4d3c809990479c
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-01-30 21:20:51 +0500

    ProductItem Image updated

--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -9,12 +9,12 @@ export default function ProductItem({ product }: { product: Product }) {
     <div className="card bg-base-300 shadow-xl mb-4">
       <figure>
         <Link href={`/product/${product.slug}`}>
-          <Image
+          <img
             src={product.image}
             alt={product.name}
             width={300}
             height={300}
-            className="object-cover h-64"
+            className="object-cover h-64 w-full"
           />
         </Link>
       </figure>
